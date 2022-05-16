c272adcc5e92650e125e3dce24820ec963b2a3df
anjalireghunath <anjalireghu98@gmail.com>
2022-05-16 19:45:45 +0530

    addstudent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Addstudent from './components/Addstudent';
 
 function App() {
   return (
-    <div></div>
+    <div><Addstudent/></div>
   );
 }
 
