6c703f80a1f7cee088315d26b0bfc416e77d9940
anjalireghunath <anjalireghu98@gmail.com>
2022-05-16 20:59:24 +0530

    serach student

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addstudent from './components/Addstudent';
+import Search_student from './components/Search_student';
 
 function App() {
   return (
-    <div><Addstudent/></div>
+    <div><Search_student/></div>
   );
 }
 
